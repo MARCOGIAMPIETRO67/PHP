ea6c600ee68e87c06a2b94f1011396511b149812
MARCOGIAMPIETRO67 <mgiampietro502@alumnos.iua.edu.ar>
2023-04-12 16:05:25 -0300

    cambios

---
+++ ejercicios/actas.php
@@ -0,0 +1,74 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$aAlumnos = array();
+$aAlumnos[] = array("nombre" => "Ana Valle", "notas" => array(7, 8));
+$aAlumnos[] = array("nombre" => "Bernabe Paz", "notas" => array(5, 7));
+$aAlumnos[] = array("nombre" => "Sebastian Aguirre", "notas" => array(6, 9));
+$aAlumnos[] = array("nombre" => "Monica Ledesma", "notas" => array(8, 9));
+
+function promediar($aNumeros){
+    $suma = 0;
+    foreach ($aNumeros as $numero){
+        $suma += $numero;
+    }
+    return $suma / count($aNumeros);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Formulario</title>
+</head>
+<body>
+<main class="container">
+        <div class="row">
+            <div class="col-12 py-5 text-center">
+                <h1>Actas</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-hover border">
+                    <thead>
+                        <tr>
+                            <th>Alumno</th>
+                            <th>Nota 1</th>
+                            <th>Nota 2</th>
+                            <th>Promedio</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sumPromedios = 0; 
+                        foreach($aAlumnos as $alumno):
+                                $promedio = promediar($alumno["notas"]);
+                                $sumPromedios += $promedio;
+                        ?>
+                        <tr>
+                            <td><?php echo $alumno["nombre"]; ?></td>
+                            <td><?php echo $alumno["notas"][0]; ?></td>
+                            <td><?php echo $alumno["notas"][1]; ?></td>
+                            <td><?php echo promediar($alumno["notas"]); ?></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h5>Promedio de la cursada: <?php echo number_format($sumPromedios / count($aAlumnos), 2, ",", "."); ?></h5>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
