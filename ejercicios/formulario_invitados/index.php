6045d99e510dba358db3e86017db57dde9bb7915
MARCOGIAMPIETRO67 <mgiampietro502@alumnos.iua.edu.ar>
2023-05-16 21:18:42 -0300

    cambios

---
+++ ejercicios/formulario_invitados/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+        <div class="col-12 py-5">
+                <h1>Lista de invitados</h1>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
